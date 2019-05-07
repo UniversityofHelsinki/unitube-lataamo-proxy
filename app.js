b7348f95721274ccc59a090e78307e7d18db17e0
Toni Räsänen <toni.rasanen@helsinki.fi>
2019-05-07 14:55:13 +0300

    LATAAMO-74 cleaning and start script

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-app.use(cors());
 const bodyParser = require('body-parser');
-const port = 3000;
 const routes = require('./api/routes');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
+const app = express();
+const port = 3000;
 const router = express.Router();
 
 routes(router);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use('/api', router);
 
-app.get('/', (req, res) => res.send({ status: 200, message: 'Alive' }));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req, res) => {
+    res.send({ status: 200, message: 'Alive' })
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});    
