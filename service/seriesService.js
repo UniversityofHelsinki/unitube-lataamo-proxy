1cace58188b4958edcbb1c5014abee735cbc59f5
vmheikki <veli-matti.heikkila@helsinki.fi>
2019-09-13 14:23:44 +0300

    contributors and acl roles null check

--- service/seriesService.js
+++ service/seriesService.js
@@ -44,9 +44,9 @@ const updateSeriesEntryById = (seriesMetadataTemplate, id, value) => {
     });
 };
 
-const updateSeriesContributorsList = (seriesMetadataTemplate, value) => {
+const updateSeriesContributorsList = (seriesMetadataTemplate, contributors) => {
     const seriesContributors = constants.SERIES_CONTRIBUTORS_TEMPLATE;
-    seriesContributors.value = value;
+    seriesContributors.value = contributors;
     return seriesMetadataTemplate[0].fields.push(seriesContributors);
 }
 
@@ -54,7 +54,7 @@ exports.openCastFormatSeriesMetadata = (metadata) => {
     let seriesMetadataTemplate = constants.SERIES_METADATA;
     updateSeriesEntryById(seriesMetadataTemplate, "title", metadata.title);
     updateSeriesEntryById(seriesMetadataTemplate, "description", metadata.description);
-    updateSeriesContributorsList(seriesMetadataTemplate, metadata.contributors);
+    metadata.contributors && metadata.contributors.length > 0 ? updateSeriesContributorsList(seriesMetadataTemplate, metadata.contributors) : '';
     return seriesMetadataTemplate;
 };
 
@@ -76,12 +76,14 @@ const updateSeriesAclList = (aclList) => {
     let seriesAclTemplate = constants.SERIES_ACL_TEMPLATE;
     let seriesACLTemplateReadEntry = constants.SERIES_ACL_TEMPLATE_READ_ENTRY;
     let seriesACLTemplateWriteEntry = constants.SERIES_ACL_TEMPLATE_WRITE_ENTRY;
-    aclList.forEach(aclRole => {
-        seriesACLTemplateReadEntry = updateAclTemplateReadEntry(seriesACLTemplateReadEntry, aclRole);
-        seriesACLTemplateWriteEntry = updateAclTemplateWriteEntry(seriesACLTemplateWriteEntry, aclRole);
-        seriesAclTemplate.push(seriesACLTemplateReadEntry);
-        seriesAclTemplate.push(seriesACLTemplateWriteEntry);
-    });
+    if(aclList) {
+        aclList.forEach(aclRole => {
+            seriesACLTemplateReadEntry = updateAclTemplateReadEntry(seriesACLTemplateReadEntry, aclRole);
+            seriesACLTemplateWriteEntry = updateAclTemplateWriteEntry(seriesACLTemplateWriteEntry, aclRole);
+            seriesAclTemplate.push(seriesACLTemplateReadEntry);
+            seriesAclTemplate.push(seriesACLTemplateWriteEntry);
+        });
+    }
     return seriesAclTemplate;
 };
 
