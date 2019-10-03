cbeec8018072c50e43e05dd4adc32cd21c473dc8
vmheikki <veli-matti.heikkila@helsinki.fi>
2019-10-03 10:42:58 +0300

    refactored parameter name

--- service/personApiService.js
+++ service/personApiService.js
@@ -3,8 +3,8 @@ const security = require('../config/security');
 
 exports.getPersons = async (query) => {
     try {
-        const iamPersonsApiPath = constants.ESB_PERSONS_PATH + encodeURI(query);
-        const response = await security.esbPersonBase.get(iamPersonsApiPath);
+        const personsPath = constants.ESB_PERSONS_PATH + encodeURI(query);
+        const response = await security.esbPersonBase.get(personsPath);
         return response.data;
     } catch (error) {
         console.log(error);
