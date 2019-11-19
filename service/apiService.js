73dcc096ee195803fcf6f15b1c1522d1ffc3efa5
vmheikki <veli-matti.heikkila@helsinki.fi>
2019-11-19 16:21:42 +0200

    changed to use video url from opencast

--- service/apiService.js
+++ service/apiService.js
@@ -300,8 +300,7 @@ exports.uploadVideo = async (filePathOnDisk, videoFilename, inboxUserSeriesId) =
 };
 
 exports.downloadVideo = async (videoUrl) => {
-    const url = 'http://localhost:8080/assets/assets/d47304d6-30db-44b1-94a7-ede78cf58ca0/9e4de2b2-2831-46c2-b1cf-99b4ba7b621e/6/fruits_on_table.mp4';
-    const response = await security.opencastBaseStream.get(url);
+    const response = await security.opencastBaseStream.get(videoUrl);
     return response;
 };
 
