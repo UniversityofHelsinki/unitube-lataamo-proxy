5b75f3d4835196d5a6d876072b4ac7f7ddeef9b7
vmheikki <veli-matti.heikkila@helsinki.fi>
2019-12-27 10:25:04 +0200

    ascending sort to persons and groups list

--- service/iamGroupsApi.js
+++ service/iamGroupsApi.js
@@ -4,5 +4,7 @@ const security = require('../config/security');
 exports.getIamGroups = async (query) => {
     const iamGroupsApiPath = constants.ESB_IAM_GROUPS_PATH + encodeURI(query);
     const response = await security.esbGroupsBase.get(iamGroupsApiPath);
-    return response.data;
+    return sortByValue(response.data);
 };
+
+const sortByValue = (iamGroups) => iamGroups.sort((a, b) => a.grpName.localeCompare(b.grpName));
