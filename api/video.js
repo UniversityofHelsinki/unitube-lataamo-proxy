336b5199c1d7dd5b644e3cf3365bf2cddb75562c
Toni Räsänen <toni.rasanen@helsinki.fi>
2020-02-25 14:48:36 +0200

    LATAAMO-393 vastaanota vtt tiedosto clientilta

--- api/video.js
+++ api/video.js
@@ -7,7 +7,10 @@ const apiService = require('../service/apiService');
 const publicationService = require('../service/publicationService');
 const logger = require('../config/winstonLogger');
 const messageKeys = require('../utils/message-keys');
-const fs = require('fs');
+const webvttParser = require('node-webvtt');
+
+
+
 
 exports.getVideoUrl = async (req, res) => {
     try {
@@ -93,6 +96,73 @@ exports.downloadVideo = async (req, res) => {
         logger.error(`Error PUT /userVideos/:id ${msg} USER ${req.user.eppn}`);
         res.json({
             message: messageKeys.ERROR_MESSAGE_FAILED_TO_DOWNLOAD_VIDEO,
+            msg
+        });
+    }
+};
+
+
+exports.uploadVideoTextTrack = async(req, res) => {
+    // https://attacomsian.com/blog/express-file-upload-multer
+    // https://www.npmjs.com/package/multer
+    logger.info('addVideoTextTrack called.');
+    try {
+        // get the vtt file from the form
+        const vttFile = req.file;
+
+        // make sure file was available
+        if (!vttFile) {
+            res.status(400).send({
+                status: false,
+                data: 'No file is selected.'
+            });
+        } else {
+            // vttFile structure:
+            //{   fieldname: 'video_webvtt_file',
+            //    originalname: 'fulica.vtt',
+            //    encoding: '7bit',
+            //    mimetype: 'text/vtt',
+            //    buffer: <Buffer xyz.....>,
+            //    size: 226 }
+
+            // do some validation to the file
+            const parsed = webvttParser.parse(vttFile.buffer.toString());
+
+            if (parsed && parsed.valid) {
+                // all ok
+                logger.info('vtt file parsed ok, next file to opencast.');
+                // TODO: send the vtt file to opencast
+                // const response = await apiService.uploadVideoTextTrack(file, eventId);
+                // res.status(response.status);
+                // res.json({message : response.statusText});
+                res.send({
+                    status: true,
+                    message: 'File received in unitube-lataamo.',
+                    data: {
+                        name: vttFile.originalname,
+                        mimetype: vttFile.mimetype,
+                        size: vttFile.size
+                    }
+                });
+            } else {
+                // not all ok
+                logger.error(`vtt file seems to be malformed, please check. USER ${req.user.eppn}`);
+
+                res.status(400);
+                const msg = 'vtt file seems to be malformed, please check.'
+                res.json({
+                    message: 'Failure in video text track upload',
+                    msg
+                });
+            }
+        }
+    } catch (err) {
+        logger.error(`addVideoTextTrack USER ${req.user.eppn}`);
+
+        res.status(500);
+        const msg = err.message;
+        res.json({
+            message: 'Failure in video text track upload',
             msg
         });
     }
