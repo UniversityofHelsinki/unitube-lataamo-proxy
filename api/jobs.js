951b0148abf8051613a4132b6a783d9548aacac4
vmheikki <veli-matti.heikkila@helsinki.fi>
2020-03-25 10:14:27 +0200

    return http statuscode 500 if no job is found

--- api/jobs.js
+++ api/jobs.js
@@ -8,16 +8,23 @@ exports.getJobStatus =  (req, res) => {
     try {
         logger.info(`GET /monitor/:jobId ${req.params.jobId}`);
         const job = jobService.getJob(req.params.jobId);
-        if (job.status === constants.JOB_STATUS_STARTED) {
-            res.status(HttpStatus.ACCEPTED);
+        if (job) {
+            if (job.status === constants.JOB_STATUS_STARTED) {
+                res.status(HttpStatus.ACCEPTED);
+            }
+            if (job.status === constants.JOB_STATUS_FINISHED) {
+                res.status(HttpStatus.CREATED)
+            }
+            if (job.status === constants.JOB_STATUS_ERROR) {
+                res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            res.json(job);
+        } else {
+            res.status(500);
+            res.json({
+                message: messageKeys.ERROR_MESSAGE_FAILED_TO_GET_JOB
+            });
         }
-        if (job.status === constants.JOB_STATUS_FINISHED) {
-            res.status(HttpStatus.CREATED)
-        }
-        if (job.status === constants.JOB_STATUS_ERROR) {
-          res.status(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        res.json(job);
     } catch (error) {
         const msg = error.message;
         res.status(500);
